20b0014fc894d9c3a5eed99bae098a5450738238
Arthur CHE <arthur@enclave.vn>
2021-10-31 22:19:41 +0700

    update chartScreen

--- src/screens/ChartScreen/index.tsx
+++ src/screens/ChartScreen/index.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native'
 const ChartScreen = () => {
     return (
         <View>
-            <Text>Chart Screen</Text>
+            <Text>Chart Screen Demo</Text>
             <Text>Apply Later</Text>
         </View>
     )
