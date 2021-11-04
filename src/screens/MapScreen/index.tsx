ffe4c1b2bd23f8c0d69a417c5010948ce3cacfb0
Arthur CHE <arthur@enclave.vn>
2021-11-04 20:19:46 +0700

    change map screen (1)

--- src/screens/MapScreen/index.tsx
+++ src/screens/MapScreen/index.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native'
 const MapScreen = () => {
     return (
         <View>
-            <Text>Map Screen</Text>
+            <Text>Map Screen 1</Text>
         </View>
     )
 }
